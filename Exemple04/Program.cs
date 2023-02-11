da232d614a15e534cfa98de10329cc6b52ee2679
New <New@example.com>
2023-02-11 21:34:53 +0500

    Пример №4

---
+++ Exemple04/Program.cs
@@ -0,0 +1,17 @@
+﻿int a=2, b = 3, c = 7;
+if(a>=b){
+    if(a >= c){
+        Console.Write("max = ");
+        Console.WriteLine(a);
+    }
+}
+else{
+    if(b >= c){
+        Console.Write("max = ");
+        Console.WriteLine(b);
+    }
+    else{
+    Console.Write("max = ");
+    Console.WriteLine(c);
+    }
+}
